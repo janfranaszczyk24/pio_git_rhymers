3de3b0e6a7a005e330d28963a01355636b584510
255585 <255585@edu.p.lodz.pl>
2026-03-30 12:55:46 +0200

    13 uporzadkowanie RhymersDemo oraz 14 usuniecie zbednych klas

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,32 @@
 package edu.kis.vh.nursery.list;
 
+class Node {
+
+    public int value;
+    private Node prev;
+    private Node next;
+
+    public Node(int i) {
+        value = i;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
+
 public class IntLinkedList {
 
     private Node last;
@@ -38,3 +65,4 @@ public class IntLinkedList {
     }
 
 }
+
